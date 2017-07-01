156d1e444f1e2a0ae59215cd7d40ee110812ebd7
Jelena Kocic <kocicjelena@gmail.com>
2017-07-01 19:21:52 +0200

    Create __init__.py

---
+++ example/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from __future__ import absolute_import
+
+__author__       = 'kocicjelena'
+__email__        = 'kocicjelena@gmail.com'
+__copyright__    = 'Copyright (c) 2017'
+__license__      = 'Apache License 2.0'
+__version__      = '0.1'
+__url__          = 'https://github.com/kocicjelena/useapi'
+__download_url__ = 'https://pypi.python.org/pypi/usemodule'
+from .usemodule import UseApi                        
