5eb5d8480973932581f51fbaecd2d21c3ac429f2
Jelena Kocic <kocicjelena@gmail.com>
2017-07-03 08:39:39 +0200

    Update __init__.py

--- usemodule/__init__.py
+++ usemodule/__init__.py
@@ -6,10 +6,9 @@ from __future__ import absolute_import
 __author__       = 'kocicjelena'
 __email__        = 'kocicjelena@gmail.com'
 __copyright__    = 'Copyright (c) 2017'
-__license__      = 'Apache License 2.0'
 __version__      = '0.1'
-__url__          = 'https://github.com/kocicjelena/useapi'
-__download_url__ = 'https://pypi.python.org/pypi/useapi'
+__url__          = 'https://github.com/kocicjelena/usemodule'
+__download_url__ = 'https://pypi.python.org/pypi/usemodule'
 __description__  = 'A Python wrapper around the use API'
 
 
